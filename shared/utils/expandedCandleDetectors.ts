70185dd368f2369149d02ec89dede4d80b7b39b1
Maiglon Alexandre Lubacheuski <maiglonl@gmail.com>
2026-06-27 19:02:45 -0300

    feat: register continuation candle detectors

--- shared/utils/expandedCandleDetectors.ts
+++ shared/utils/expandedCandleDetectors.ts
@@ -1,11 +1,15 @@
 import type { PatternDetector } from '#shared/utils/detectors/PatternDetector';
 import { BearishKickerDetector } from '#shared/utils/detectors/candle/bearishKicker';
+import { BearishSeparatingLinesDetector } from '#shared/utils/detectors/candle/bearishSeparatingLines';
 import { BullishKickerDetector } from '#shared/utils/detectors/candle/bullishKicker';
 import { BullishMarubozuDetector } from '#shared/utils/detectors/candle/bullishMarubozu';
+import { BullishSeparatingLinesDetector } from '#shared/utils/detectors/candle/bullishSeparatingLines';
 import { DragonflyDojiDetector } from '#shared/utils/detectors/candle/dragonflyDoji';
+import { FallingThreeMethodsDetector } from '#shared/utils/detectors/candle/fallingThreeMethods';
 import { GravestoneDojiDetector } from '#shared/utils/detectors/candle/gravestoneDoji';
 import { LongLeggedDojiDetector } from '#shared/utils/detectors/candle/longLeggedDoji';
 import { RedMarubozuDetector } from '#shared/utils/detectors/candle/redMarubozu';
+import { RisingThreeMethodsDetector } from '#shared/utils/detectors/candle/risingThreeMethods';
 import { SpinningTopDetector } from '#shared/utils/detectors/candle/spinningTop';
 import { ThreeBlackCrowsDetector } from '#shared/utils/detectors/candle/threeBlackCrows';
 import { ThreeWhiteSoldiersDetector } from '#shared/utils/detectors/candle/threeWhiteSoldiers';
@@ -22,5 +26,9 @@ export function createExpandedCandleDetectors(): PatternDetector[] {
     new BearishKickerDetector(),
     new ThreeWhiteSoldiersDetector(),
     new ThreeBlackCrowsDetector(),
+    new RisingThreeMethodsDetector(),
+    new FallingThreeMethodsDetector(),
+    new BullishSeparatingLinesDetector(),
+    new BearishSeparatingLinesDetector(),
   ];
 }
