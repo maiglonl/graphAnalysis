f87e012af371fd643f2dfadd656f6c72f2626ed9
Maiglon Alexandre Lubacheuski <maiglonl@gmail.com>
2026-06-27 19:09:41 -0300

    feat: register gap detectors

--- shared/utils/expandedCandleDetectors.ts
+++ shared/utils/expandedCandleDetectors.ts
@@ -1,15 +1,21 @@
 import type { PatternDetector } from '#shared/utils/detectors/PatternDetector';
+import { BearishBreakawayGapDetector } from '#shared/utils/detectors/candle/bearishBreakawayGap';
 import { BearishKickerDetector } from '#shared/utils/detectors/candle/bearishKicker';
 import { BearishSeparatingLinesDetector } from '#shared/utils/detectors/candle/bearishSeparatingLines';
+import { BullishBreakawayGapDetector } from '#shared/utils/detectors/candle/bullishBreakawayGap';
 import { BullishKickerDetector } from '#shared/utils/detectors/candle/bullishKicker';
 import { BullishMarubozuDetector } from '#shared/utils/detectors/candle/bullishMarubozu';
 import { BullishSeparatingLinesDetector } from '#shared/utils/detectors/candle/bullishSeparatingLines';
 import { DragonflyDojiDetector } from '#shared/utils/detectors/candle/dragonflyDoji';
 import { FallingThreeMethodsDetector } from '#shared/utils/detectors/candle/fallingThreeMethods';
+import { GapFillBearishDetector } from '#shared/utils/detectors/candle/gapFillBearish';
+import { GapFillBullishDetector } from '#shared/utils/detectors/candle/gapFillBullish';
 import { GravestoneDojiDetector } from '#shared/utils/detectors/candle/gravestoneDoji';
 import { LongLeggedDojiDetector } from '#shared/utils/detectors/candle/longLeggedDoji';
 import { RedMarubozuDetector } from '#shared/utils/detectors/candle/redMarubozu';
 import { RisingThreeMethodsDetector } from '#shared/utils/detectors/candle/risingThreeMethods';
+import { RunawayGapDownDetector } from '#shared/utils/detectors/candle/runawayGapDown';
+import { RunawayGapUpDetector } from '#shared/utils/detectors/candle/runawayGapUp';
 import { SpinningTopDetector } from '#shared/utils/detectors/candle/spinningTop';
 import { ThreeBlackCrowsDetector } from '#shared/utils/detectors/candle/threeBlackCrows';
 import { ThreeWhiteSoldiersDetector } from '#shared/utils/detectors/candle/threeWhiteSoldiers';
@@ -30,5 +36,11 @@ export function createExpandedCandleDetectors(): PatternDetector[] {
     new FallingThreeMethodsDetector(),
     new BullishSeparatingLinesDetector(),
     new BearishSeparatingLinesDetector(),
+    new BullishBreakawayGapDetector(),
+    new BearishBreakawayGapDetector(),
+    new RunawayGapUpDetector(),
+    new RunawayGapDownDetector(),
+    new GapFillBullishDetector(),
+    new GapFillBearishDetector(),
   ];
 }
