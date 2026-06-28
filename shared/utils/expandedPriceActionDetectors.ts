cf3da2b494ba47ba9df32e2d889c0c199b498ec6
Maiglon Alexandre Lubacheuski <maiglonl@gmail.com>
2026-06-28 10:41:10 -0300

    feat: register head and shoulders detectors

--- shared/utils/expandedPriceActionDetectors.ts
+++ shared/utils/expandedPriceActionDetectors.ts
@@ -1,6 +1,8 @@
 import type { PatternDetector } from '#shared/utils/detectors/PatternDetector';
 import { DoubleBottomDetector } from '#shared/utils/detectors/priceAction/doubleBottom';
 import { DoubleTopDetector } from '#shared/utils/detectors/priceAction/doubleTop';
+import { HeadAndShouldersDetector } from '#shared/utils/detectors/priceAction/headAndShoulders';
+import { InverseHeadAndShouldersDetector } from '#shared/utils/detectors/priceAction/inverseHeadAndShoulders';
 import { TripleBottomDetector } from '#shared/utils/detectors/priceAction/tripleBottom';
 import { TripleTopDetector } from '#shared/utils/detectors/priceAction/tripleTop';
 
@@ -10,5 +12,7 @@ export function createExpandedPriceActionDetectors(): PatternDetector[] {
     new DoubleBottomDetector(),
     new TripleTopDetector(),
     new TripleBottomDetector(),
+    new HeadAndShouldersDetector(),
+    new InverseHeadAndShouldersDetector(),
   ];
 }
